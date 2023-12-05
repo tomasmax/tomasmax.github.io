d2523d8aea3ca653c1667d1c2a1e01f64bd749d9
Tomas Madariaga <critimass@gmail.com>
2023-12-05 13:59:45 +0100

    feat: add GA4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://tomasmax.github.io",
+  integrations: [
+    mdx(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
